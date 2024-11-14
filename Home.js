017f663a49ad9fde309048777a2e3a1f4698d2b0
Goutham <727723eucs063@skcet.ac.in>
2024-11-14 11:21:53 +0530

    Add files via upload

--- Home.js
+++ Home.js
@@ -1,13 +1,9 @@
-// src/pages/Home.js
 import React from 'react';
-import '../styles/Home.css';
-const Home = () => {
-  return (
-    <div className="home">
-      <h2>Welcome to the Inventory Management System</h2>
-      <p>Manage your inventory efficiently and effectively.</p>
-    </div>
-  );
-};
+
+const Home = () => (
+  <div>
+    <h1>Welcome to the Inventory Management System</h1>
+  </div>
+);
 
 export default Home;
